f21df4307e8c2ed08a325b0a355b53572ea38852
Mekhi M <mmcfowler@brooklynsteamcenter.org>
2023-05-17 14:52:30 -0400

    i hate vue

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
 import {ref, watch} from 'vue'
 let todos = ref(JSON.parse(window.localStorage.getItem('todos')))
 let newTodo = ref('')
-
-
+let filter = ref('all')
 
 function todoButton() {
   todos.value.push({
@@ -17,22 +16,38 @@ function deleteTodo(index) {
 }
 
 watch(todos, function(value) {
-  window.localStorage.setItem('todos', JSON.stringify(value))
-}, {deep: true})
+  window.localStorage.setItem('todos', JSON.stringify(value))}, {deep: true})
+
+function todoFilter(todo) {
+  if (filter.value === 'active') {
+    return todo.complete === false;
+  } else if (filter.value === 'completed') {
+    return todo.complete === true;
+  }
+  return true;
+}
+
 </script>
 
 <template>
 <h1 id="welcome">My Todo</h1> 
 
+
 <div id="blackbox">
   <ul>
-    <li v-for="(todo, index) in todos" :class="{completed: todo.complete}" class="flex-container">
+    <li v-for="(todo, index) in todos.filter(todoFilter)" :class="{completed: todo.complete}" class="flex-container">
       <input class="checkboxItem" type="checkbox" checked="checked" v-model="todo.complete"> 
     {{ todo.text }}
     <button @click="$event => deleteTodo(index)" class="deleteItem">X</button>
 
     </li>
   </ul>
+  <li id="buttonThing">
+  <button class="button" :class="{ 'active': filter === 'all' }" @click="filter = 'all'">All</button>
+  <button class="button" :class="{ 'active': filter === 'active' }" @click="filter = 'active'">Active</button>
+  <button class="button" :class="{ 'active': filter === 'completed' }" @click="filter = 'completed'">Completed</button>
+  </li> 
+
 </div>
 
 <input v-model="newTodo" @keydown.enter="todoButton" id="todoInput">
